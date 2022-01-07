c39cb78444815707071ebd2d139a486e6f0b4051
Ankit Dhopate <ankit.dhopate@gmail.com>
2022-01-07 20:56:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let img = document.getElementById("zippy_img");
+let img_1 = document.getElementById("img_1");
+let img_2 = document.getElementById("img_2");
+let img_3 = document.getElementById("img_3");
+let img_4 = document.getElementById("img_4");
+let img_5 = document.getElementById("img_5");
+
+img_1.addEventListener("click", () => {
+  img.src = "./Images/life_at_zippy_1.png";
+});
+
+img_2.addEventListener("click", () => {
+  img.src = "./Images/life_at_zippy_2.png";
+});
+
+img_3.addEventListener("click", () => {
+  img.src = "./Images/life_at_zippy_3.png";
+});
+
+img_4.addEventListener("click", () => {
+  img.src = "./Images/life_at_zippy_4.png";
+});
+
+img_5.addEventListener("click", () => {
+  img.src = "./Images/life_at_zippy_5.png";
+});
